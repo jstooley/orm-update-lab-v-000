681aba6ac5fd2cef05a166d8842dced302e4a4b8
Justin Tooley <jstooley823@gmail.com>
2017-10-29 19:27:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,9 @@ class Student
   end
 
   def save
+    sql = <<-SQL
+    INSERT INTO students (name,grade) VALUES (?,?)
+    SQL
   end
 
   # Remember, you can access your database connection anywhere in this class
