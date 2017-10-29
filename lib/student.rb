41197316ff9b6e691987bd21c937197a701e0f2c
Justin Tooley <jstooley823@gmail.com>
2017-10-29 19:25:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
     sql = <<-SQL
     DROP TABLE IF EXISTS students
     SQL
-    db[:conn].execute(sql)
+    DB[:conn].execute(sql)
   end
 
   # Remember, you can access your database connection anywhere in this class
