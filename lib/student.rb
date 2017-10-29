89b9a157d01b3820cd3f8e3e53f58268acc4e93a
Justin Tooley <jstooley823@gmail.com>
2017-10-29 19:39:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ class Student
     end
   end
 
-  def self.create
+  def self.create(:name,:grade)
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
