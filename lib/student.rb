bf8e4e9817db013e60b84b3aa1962f1913608844
Justin Tooley <jstooley823@gmail.com>
2017-10-29 20:12:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE name = #{name}
     SQL
-    DB[:conn].execute(sql,name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
