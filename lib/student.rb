915cbf9c863ac14c8f2082db65304449640773c7
Justin Tooley <jstooley823@gmail.com>
2017-10-29 20:14:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,9 +65,9 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
 
   # Remember, you can access your database connection anywhere in this class
