f5a26d902a65d28a20436262b0b8346c1a96cb8a
Justin Tooley <jstooley823@gmail.com>
2017-10-29 20:07:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,10 @@ class Student
     new_student
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students WHERE name = #{name}
+    SQL
   end
 
   # Remember, you can access your database connection anywhere in this class
